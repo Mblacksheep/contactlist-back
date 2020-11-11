bdb15ca3b26e26966e2bd7bddcb35fb23de18d8a
michel albino <michel@bitlab@com.br>
2020-11-11 14:02:59 -0300

    Criacao do controller de person e metodo create

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
 {
-    //
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response(["error" => $validator->errors()]);
+        }
+
+        $person = new Person();
+        $person->name = $request->name;
+        $person->save();
+
+        return response(["person" => $person], 200);
+    }
 }
