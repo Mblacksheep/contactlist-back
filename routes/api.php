61851a0eca45a471558ec798572471558e707cdb
michel albino <michel@bitlab@com.br>
2020-11-12 02:48:16 -0300

    Api balancedBrackets

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use App\Http\Controllers\PersonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('person/',  [PersonController::class, 'all']);
+Route::get('person',  [PersonController::class, 'all']);
 Route::get('person/{id}',  [PersonController::class, 'byId']);
 Route::post('person/create',  [PersonController::class, 'create']);
 Route::post('person/delete',  [PersonController::class, 'delete']);
 
-Route::get('contact/',  [ContactController::class, 'all']);
+Route::get('contact',  [ContactController::class, 'all']);
 Route::get('contact/{id}',  [ContactController::class, 'byId']);
 Route::post('contact/create',  [ContactController::class, 'create']);
 Route::post('contact/delete',  [ContactController::class, 'delete']);
+
+Route::post('balanced-brackets',  [BalancedBracketsController::class, 'balancedBrackets']);
